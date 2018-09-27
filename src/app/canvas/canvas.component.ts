60483bbd4a27e4ea13b33af4b544d34c65eaadf4
James Bliss <james.bliss@outlook.com>
2018-09-27 22:26:07 +0100

    Adding canvas component

---
+++ src/app/canvas/canvas.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas',
+  //templateUrl: './canvas.component.html',
+  template: '<h2>AAAARRRRRR</h2>',
+  styleUrls: ['./canvas.component.css']
+})
+export class CanvasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
